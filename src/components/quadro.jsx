0da708ce0e20202f497b4d45dd1664855fe982c6
Mariane Fatima <fmari6979@gmail.com>
2025-08-05 16:06:21 -0300

    componente quadro feito

---
+++ src/components/quadro.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledQuadro = styled.div`
+  background-color: #75a0d1;
+  box-shadow: 0px 4px 5px 4px rgba(0, 0, 0, 0.149);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 40vh;
+  margin: 0 12%;
+  border-radius: 5%;
+`;
+
+export const StyledPParaQuadro = styled.p`
+  margin: 5%;
+  font-weight: 20px;
+  color: white;
+  font-family: "Nunito", sans-serif;
+  text-indent: 0%;
+`;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,26 +1,28 @@
 import { Footer } from "src/components/footer";
 import { TelaBase } from "src/components/telaBase";
 import { Header } from "src/components/header";
+import { MyButton } from "../components/myButton";
+import { StyledQuadro } from "../components/quadro";
+import { StyledPParaQuadro } from "../components/quadro";
 export function Login() {
-    return (
-        <div className="login-container">
-            <TelaBase>
-                <Header/>
-                <h1>Login Page</h1>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" name="username" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    <button type="submit">Login</button>
-                </form>
-                <Footer/>
-            </TelaBase>
-        </div>
-        
-    );
+  return (
+    <div className="login-container">
+      <TelaBase>
+        <Header />
+        <h1>Acesse</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <MyButton>Entrar</MyButton>
+        </form>
+        <Footer />
+      </TelaBase>
+    </div>
+  );
 }
