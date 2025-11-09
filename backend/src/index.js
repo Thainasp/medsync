288701ac0c20d4f1efe414888ed9c5435cf62508
Thaina de Souza <souza.thainap@gmail.com>
2025-11-09 14:39:11 -0300

    estrutura backend criada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+const userRoutes = require('./src/routes/userRoutes');
+const prescriptionRoutes = require('./src/routes/prescriptionRoutes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/prescriptions', prescriptionRoutes);
+
 const port = 3001;
 
 app.get('/', (req, res) => {
